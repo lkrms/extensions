95036c8d7da100535103537f2e0ef85e6c9e2dcb
Luke Arms <code@lukearms.com.au>
2013-11-07 13:11:04 +1100

    Added stub for DB connection settings.

---
+++ osTicket/common.php
@@ -0,0 +1,11 @@
+<?php
+
+// this has nothing to do with any installed instance of osTicket; we don't have any osTicket code dependencies
+if ( ! defined("OST_ROOT"))
+{
+    define("OST_ROOT", dirname(__file__));
+}
+
+require_once (OST_ROOT . "/config.php");
+
+?>
---
+++ osTicket/config.default.php
@@ -0,0 +1,10 @@
+<?php
+
+// If running on the same host as osTicket itself, you can copy and paste these settings from <include/ost-config.php>.
+// Alternatively, create a dedicated read-only MySQL user and provide its credentials here.
+define("DBHOST", "localhost");
+define("DBNAME", "osticket");
+define("DBUSER", "osticket");
+define("DBPASS", "PASSWORD");
+
+?>
