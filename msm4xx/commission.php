f1f2407cc86604c799d0a8ea05783f1a0baa727c
Luke Arms <code@lukearms.com.au>
2014-07-29 17:22:16 +1000

    Improved output formatting.

--- msm4xx/commission.php
+++ msm4xx/commission.php
@@ -20,30 +20,30 @@ $mac          = strtolower(str_replace(":", "", $ap["mac"]));
 $secret       = $ap["secret"];
 $configUrl    = MSM4XX_CONFIG_URL . "?mac={$mac}&secret={$secret}";
 $firmwareUrl  = MSM4XX_FIRMWARE_URL . $ap["model"] . ".cim";
-$pw = MSM4XX_ADMIN_PASSWORD;
+$pw           = MSM4XX_ADMIN_PASSWORD;
 
 // let's assume the AP was factory reset 0.2 seconds ago ;)
-echo "Waiting 120 seconds (in case the AP hasn't booted yet)...";
+echo "Waiting 120 seconds (in case the AP hasn't booted yet)...\n";
 sleep(120);
 
 // factory-reset APs boot in managed mode
-echo "Switching operational mode (to autonomous)...";
+echo "Switching operational mode (to autonomous)...\n";
 system("SSHPASS=admin sshpass -e ssh -o StrictHostKeyChecking=no -l admin $ip 'enable; switch operational mode'");
 
 // switching to autonomous mode can take more than 2 minutes
-echo "Waiting 180 seconds (for the AP to boot in autonomous mode)...";
+echo "Waiting 180 seconds (for the AP to boot in autonomous mode)...\n";
 sleep(180);
 
 // TODO: add a firmware version check (currently upgrades regardless)
-echo "Setting admin password and upgrading firmware...";
+echo "Setting admin password and upgrading firmware...\n";
 system("SSHPASS=admin sshpass -e ssh -o StrictHostKeyChecking=no -l admin $ip 'enable; config; username admin $pw; no firmware-update automatic; firmware-update uri \"$firmwareUrl\"; firmware-update start'");
 
 // it takes less than two minutes to install firmware and reboot
-echo "Waiting 120 seconds (for the AP to boot with new firmware)...";
+echo "Waiting 120 seconds (for the AP to boot with new firmware)...\n";
 sleep(120);
 
 // finally, configure the AP
-echo "Deploying configuration...";
+echo "Deploying configuration...\n";
 system("SSHPASS=$pw sshpass -e ssh -o StrictHostKeyChecking=no -l admin $ip 'enable; config; no config-update automatic; config-update operation restore; config-update uri \"$configUrl\"; config-update start'");
 
 ?>
