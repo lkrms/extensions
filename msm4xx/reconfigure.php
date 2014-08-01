b34c9aaac79d2dea4a580b80b8910a72303f5f50
Luke Arms <code@lukearms.com.au>
2014-08-01 17:47:11 +1000

    Added -all option to reconfigure.php

--- msm4xx/reconfigure.php
+++ msm4xx/reconfigure.php
@@ -9,20 +9,35 @@ if (php_sapi_name() != "cli")
 // pull in our settings
 require_once (dirname(__file__) . "/config.php");
 
-if ($argc != 2 || ! isset($MSM4XX_AP[$apName = $argv[1]]))
+if ($argc != 2 || ($argv[1] != "-all" && ! isset($MSM4XX_AP[$argv[1]])))
 {
-    exit ("Usage: reconfigure.php <AP_NAME>");
+    exit ("Usage: reconfigure.php <AP_NAME>|-all");
 }
 
-$ap         = $MSM4XX_AP[$apName];
-$ip         = $ap["ip"];
-$mac        = strtolower(str_replace(":", "", $ap["mac"]));
-$secret     = $ap["secret"];
-$configUrl  = MSM4XX_CONFIG_URL . "?mac={$mac}&secret={$secret}";
-$pw         = MSM4XX_ADMIN_PASSWORD;
+if ($argv[1] == "-all")
+{
+    $apNames = array_keys($MSM4XX_AP);
+}
+else
+{
+    $apNames = array($argv[1]);
+}
 
-// NB: this will trigger a reboot
-echo "Deploying configuration...\n";
-system("SSHPASS=$pw sshpass -e ssh -o StrictHostKeyChecking=no -l admin $ip 'enable; config; no config-update automatic; config-update operation restore; config-update uri \"$configUrl\"; config-update start'");
+$pw = MSM4XX_ADMIN_PASSWORD;
+
+foreach ($apNames as $apName)
+{
+    $ap         = $MSM4XX_AP[$apName];
+    $ip         = $ap["ip"];
+    $mac        = strtolower(str_replace(":", "", $ap["mac"]));
+    $secret     = $ap["secret"];
+    $configUrl  = MSM4XX_CONFIG_URL . "?mac={$mac}&secret={$secret}";
+
+    // NB: this will trigger a reboot
+    echo "Deploying configuration to $apName at $ip...\n";
+    system("SSHPASS=$pw sshpass -e ssh -o StrictHostKeyChecking=no -l admin $ip 'enable; config; no config-update automatic; config-update operation restore; config-update uri \"$configUrl\"; config-update start'");
+}
+
+// PRETTY_NESTED_ARRAYS,0
 
 ?>
